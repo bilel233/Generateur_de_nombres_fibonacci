06124ae24fb20a460f8e80795bdd472ae8b71fa4
bilel233 <bilel.mjdb@gmail.com>
2023-11-15 10:24:56 +0100

    Code de la fonction + ajout des tests

--- main.py
+++ main.py
@@ -4,3 +4,20 @@
 # puis on le genere
 # enfin, on affiche le resultat souhaite
 
+print("========================================")
+
+print("fonction fibonacci")
+
+def fibonacci(n):
+    """renvoie le n-ieme terme de la suite de fibonacci"""
+
+    if n == 0:
+        return 0        # renvoie le premier terme
+    elif n == 1:
+        return 1        # renvoie le second terme
+    return fibonacci(n-1)+fibonacci(n-2)
+
+# jeux de tests
+print(fibonacci(2))
+print(fibonacci(3))
+
